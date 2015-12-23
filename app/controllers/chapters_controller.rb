57e3d6e3518569bf1bd3102bf5990a0ded0fe73d
sarvin <scoachbuilder@dealersocket.com>
2015-12-22 23:16:23 -0600

    updated create, added show, index and private methods

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -3,6 +3,24 @@ class ChaptersController < ApplicationController
 	end
 
 	def create
-		render plain: params[:chapter].inspect
+		@chapter = Chapter.new(chapter_params)
+		#render plain: params[:chapter].inspect
+		#@chapter = Chapter.new(params.require(:chapter).permit(:name, :state))
+
+		@chapter.save
+		redirect_to @chapter
 	end
+
+	def show
+		@chapter = Chapter.find(params[:id])
+	end
+
+	def index
+		@chapters = Chapter.all
+	end
+
+	private
+		def chapter_params
+			params.require(:chapter).permit(:name, :state)
+		end
 end
