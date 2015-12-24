abf5bd181db7986405e4a3b6c6885079c1b17f95
sarvin <scoachbuilder@dealersocket.com>
2015-12-23 23:11:53 -0600

    restful destroy, add, update, form partial for reuse in template

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,23 +1,49 @@
 class ChaptersController < ApplicationController
 	def new
+		@chapter = Chapter.new
 	end
 
 	def create
 		@chapter = Chapter.new(chapter_params)
+
+		if @chapter.save
+			redirect_to @chapter
+		else
+			render 'new'
+		end
+
 		#render plain: params[:chapter].inspect
 		#@chapter = Chapter.new(params.require(:chapter).permit(:name, :state))
+	end
 
-		@chapter.save
-		redirect_to @chapter
+	def index
+		@chapters = Chapter.all
+	end
+
+	def edit
+		@chapter = Chapter.find(params[:id])
 	end
 
 	def show
 		@chapter = Chapter.find(params[:id])
 	end
 
-	def index
-		@chapters = Chapter.all
+	def update
+		@chapter = Chapter.find(params[:id])
+
+		if @chapter.update(chapter_params)
+			redirect_to @chapter
+		else
+			render 'edit'
+		end
 	end
+
+def destroy
+	@chapter = Chapter.find(params[:id])
+	@chapter.destroy
+
+	redirect_to chapters_path
+end
 
 	private
 		def chapter_params
--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,7 +1,5 @@
 class Chapter < ActiveRecord::Base
 	validates :name, presence: true,
-		format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },
 		uniqueness: true
-	validates :state, presence: true,
-		format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+	validates :state, presence: true
 end
