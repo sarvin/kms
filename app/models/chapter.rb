500cc4ee3c0d722fefb17b7b7019399ae9e71b6f
sarvin <scoachbuilder@dealersocket.com>
2015-12-23 22:17:11 -0600

    chapter model name must be unique

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,6 +1,7 @@
 class Chapter < ActiveRecord::Base
 	validates :name, presence: true,
-		format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+		format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },
+		uniqueness: true
 	validates :state, presence: true,
 		format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 end
