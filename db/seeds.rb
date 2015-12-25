2e9effee197b81c7cb35ba40d75d29efa7e92482
sarvin <scoachbuilder@dealersocket.com>
2015-12-24 21:09:06 -0600

    while seeding db with states sync file to db and db to file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,29 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
 
+state_name = []
+
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'us_state_table.csv'))
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
 	#puts row.to_hash
+	state = State.find_by name: row['name']
 
-	state = State.new
-	state.name = row['name']
+	unless state
+		puts "State not found in database. Creating new state with name #{row['name']}"
+		state = State.new
+		state.name = row['name']
+	end
+
 	state.abbreviated_name = row['abbreviation']
 	state.census_division_name = row['census_division_name']
 	state.save
 
 	puts "#{state.name}, #{state.abbreviated_name} saved"
+
+	state_name.push(state.name)
 end
+
+# Check for rows exising in table that don't exist in CSV file
+states_to_remove = State.where.not(name: state_name)
+states_to_remove.each { |state_to_remove| state_to_remove.destroy}
