6c611b59caf85bd3b7e357819d7c2a05ac358116
sarvin <scoachbuilder@dealersocket.com>
2015-12-26 23:34:58 -0600

    move to precompiling assets by controller, removed asset path, css for home view

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Compile CSS for home controller
+# Rails.application.config.assets.precompile += ["#{home}.css"]
+%w( home ).each do |controller|
+	Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css"]
+end
