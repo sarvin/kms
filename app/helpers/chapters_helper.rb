1d751a10ecefd66773edb4b62147861b56b1218c
sarvin <scoachbuilder@dealersocket.com>
2016-01-02 01:03:44 -0600

    chapter view helper

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,2 +1,20 @@
 module ChaptersHelper
+	def sort_chapters_by_state chapters
+		chapter_sort_hash = {}
+
+		chapters.each do |chapter|
+			if chapter.state
+				chapter_sort_hash[chapter.state.name] = chapter
+			else
+				chapter_sort_hash[chapter.name] = chapter
+			end
+		end
+
+		sorted_chapters = []
+		chapter_sort_hash.sort_by{|key, chapter| key.downcase}.each do |key, chapter|
+			sorted_chapters.push(chapter)
+		end
+
+		return sorted_chapters
+	end
 end
