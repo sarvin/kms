6a539efeec92e078019aed62727da3c63d3b3682
sarvin <scoachbuilder@dealersocket.com>
2016-01-02 00:05:36 -0600

    chapter model doesn't require a state

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,7 +1,7 @@
 class Chapter < ActiveRecord::Base
 	validates :name, presence: true,
 		uniqueness: true
-	validates :state_id, presence: true
+	#validates :state_id, presence: true
 	belongs_to :state
 	has_one :page, :as => :pageable, dependent: :destroy
 	#accepts_nested_attributes_for :page
