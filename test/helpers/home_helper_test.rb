c70d38a7bff8d2c6d20317f4a4e768b8bc7b87ae
sarvin <scoachbuilder@dealersocket.com>
2016-01-04 23:11:25 -0600

    home_helper class has methods to generate links for navigation on public site, unit tests for home_helper class

--- test/helpers/home_helper_test.rb
+++ test/helpers/home_helper_test.rb
@@ -4,27 +4,62 @@ class HomeHelperTest < ActionView::TestCase
   include HomeHelper
  
   test "should return array containing Chapter, State and Page objects" do
-		menu_items = get_menu_items
+		items = get_main_navigation_items
 
-		assert menu_items.map{|menu_item| menu_item.class.name}.uniq.sort.join() == 'ChapterPageState'
+		assert items.map{|item| item.class.name}.uniq.sort.join() == 'ChapterPageState'
   end
 
-  test "first element in sorted menu items should be home page" do
-		menu_items = get_menu_items
+  test "Page object is not pageable" do
+		items = get_main_navigation_items
 
-		ordered_menu_items = order_menu_items(menu_items)
+		items.map.each do |item|
+			if item.instance_of?(Page)
+				assert item.pageable_id? == false
+			end
+		end
+  end
+
+	test "State object has a Chapter associated with it" do
+		items = get_main_navigation_items
+
+		items.map.each do |item|
+			if item.instance_of?(State)
+				assert item.chapter_ids.count != 0
+			end
+		end
+	end
+
+	test "Chapter object does not belong to a State" do
+		items = get_main_navigation_items
+
+		items.map.each do |item|
+			if item.instance_of?(Chapter)
+				assert_not item.state_id?
+			end
+		end
+	end
+
+  test "first element in sorted menu items should be home page" do
+		menu_items = get_main_navigation_items
+		ordered_menu_items = sort_navigation_items_for_main_navigation(menu_items)
 
 		assert ordered_menu_items.first.instance_of?(Page)
 		assert ordered_menu_items.first.title == 'home'
   end
 
   test "last element in sorted menu items should be history page" do
-		menu_items = get_menu_items
-
-		ordered_menu_items = order_menu_items(menu_items)
+		menu_items = get_main_navigation_items
+		ordered_menu_items = sort_navigation_items_for_main_navigation(menu_items)
 
 		assert ordered_menu_items.last.instance_of?(Page)
 		assert ordered_menu_items.last.title == 'history'
-		#ordered_menu_items.each{|menu_item| puts menu_item.inspect}
+  end
+
+  test "generate_menu_item_info returns proper urls" do
+		menu_items = get_main_navigation_items
+		ordered_menu_items = sort_navigation_items_for_main_navigation(menu_items)
+		links = convert_main_navigation_items_to_links(ordered_menu_items)
+		#puts links.inspect
+		assert true
   end
 end
