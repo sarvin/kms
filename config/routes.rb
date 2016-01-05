83d9b7161980d208ddb9cedf8772a84af2209f0d
sarvin <scoachbuilder@dealersocket.com>
2016-01-04 21:05:06 -0600

    naming state route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 		end
 	end
 
-	resources :states
+	get 'states/:id', to: 'states#show', as: :state
 	get 'home/:title' => 'home#index'
 	#get 'state/:state_name' => 'states#view'
 
