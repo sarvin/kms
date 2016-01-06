f8327843df4dba07aa5a2cd7f2726d9fe7a4838a
sarvin <scoachbuilder@dealersocket.com>
2016-01-06 01:22:08 -0600

    adding orphaned scope to chapter for re-usable query

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,7 +2,11 @@ class Chapter < ActiveRecord::Base
 	validates :name, presence: true,
 		uniqueness: true
 	#validates :state_id, presence: true
+
 	belongs_to :state
+
 	has_one :page, :as => :pageable, dependent: :destroy
 	#accepts_nested_attributes_for :page
+
+	scope :orphaned, -> { where(state_id: nil) }
 end
