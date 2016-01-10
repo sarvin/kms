0c2a9449c24a83b6c5e81b7cb800db23fe9e6212
sarvin <scoachbuilder@dealersocket.com>
2016-01-10 14:05:18 -0600

    make web console accesible from local environment while rails app runs in vagrant box

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,7 @@ Rails.application.configure do
 	#
 	# In production, :host should be set to the actual host of your application. 
 	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # https://github.com/rails/web-console#configuration
+  config.web_console.whitelisted_ips = '10.0.2.2'
 end
