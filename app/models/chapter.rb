c5c813fa4e80dce695245fb2f2cbe8a2c9c3cfd9
sarvin <scoachbuilder@dealersocket.com>
2016-01-10 02:28:32 -0600

    model has new scoped query, tests for scoped queries

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -8,5 +8,6 @@ class Chapter < ActiveRecord::Base
 	has_one :page, :as => :pageable, dependent: :destroy
 	#accepts_nested_attributes_for :page
 
+	scope :adopted, -> { where.not(state_id: nil) }
 	scope :orphaned, -> { where(state_id: nil) }
 end
--- test/models/chapter_test.rb
+++ test/models/chapter_test.rb
@@ -4,4 +4,20 @@ class ChapterTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  test "orphaned Chapter objects don't belong to a State object" do
+    chapters = Chapter.orphaned
+
+    chapters.each do |chapter|
+				assert chapter.state.nil? == true, "State associated with chapter #{chapter.id}"
+    end
+  end
+
+  test "adopted Chapter objects do belong to a State object" do
+    chapters = Chapter.adopted
+
+    chapters.each do |chapter|
+				assert chapter.state.nil? == false, "State not associated with chapter: #{chapter.inspect}"
+    end
+  end
 end
