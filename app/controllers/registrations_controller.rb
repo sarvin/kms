b3f9034494463fe8c0170f4b3a849c5c3ab8c360
sarvin <scoachbuilder@dealersocket.com>
2016-01-14 00:57:12 -0600

    link to where I got this idea

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+  # http://stackoverflow.com/questions/5370164/disabling-devise-registration-for-production-environment-only
+
   def new
     flash[:info] = 'Registrations are not open yet, but please check back soon'
     redirect_to root_path
