aafd4675bcf45326fc977dd94ec18c6979354adc
sarvin <scoachbuilder@dealersocket.com>
2016-01-17 16:26:14 -0600

    edit user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < Admin::BaseController
+  ### A frequent practice is to place the standard CRUD actions in each
+  ### controller in the following order: index, show, new, edit, create, update
+  ### and destroy
   def index
     @chapters = Chapter.all
   end
@@ -6,4 +9,23 @@ class Admin::UsersController < Admin::BaseController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+	def update
+    @user = User.find(params[:id])
+
+		if @user.update(user_params)
+      redirect_to [:admin, @user]
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name_first, :name_last, :chapter_id, :email)
+    end
 end
