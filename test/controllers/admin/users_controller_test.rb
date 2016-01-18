d9a6d25f8955b8814a4e0a66866c16250a63ad7c
sarvin <scoachbuilder@dealersocket.com>
2016-01-18 00:42:06 -0600

    test for admin/users#new action

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -13,4 +13,17 @@ class Admin::UsersControllerTest < ActionController::TestCase
     assert_response :success, "Should be successfully viewing admin/users"
     assert_not_nil assigns(:chapters)
   end
+
+  test "should get new" do
+    @user = User.take
+    @user.confirm
+
+    sign_in :user, @user
+
+    get :new
+    assert_response :success, "Should be successfully viewing admin/users"
+    assert_not_nil assigns(:user)
+
+    assert_select '#user_password', @user.password
+  end
 end
