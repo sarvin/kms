d5a78176f06eb4fb4372eea8dd4401f9bc0ee3ab
sarvin <scoachbuilder@dealersocket.com>
2016-01-17 20:20:39 -0600

    fixed indentation to match ruby style guide, moved method declaration, scope and associations to match ruby style guide

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,10 +1,10 @@
 class State < ActiveRecord::Base
-	has_many :chapters
-	has_one :page, :as => :pageable
+  has_many :chapters
+  has_one :page, :as => :pageable
 
-	def titleize_name
-		name.titleize
-	end
+  scope :populated, -> { joins(:chapters).uniq }
 
-	scope :populated, -> { joins(:chapters).uniq }
+  def titleize_name
+    name.titleize
+  end
 end
