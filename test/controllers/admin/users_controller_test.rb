b611981b49a7f0f8c16d035e7690e8cd708713c5
sarvin <scoachbuilder@dealersocket.com>
2016-01-18 00:29:11 -0600

    initial unit tests for admin user view

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  test "should get index" do
+    @user = User.take
+    @user.confirm
+
+    sign_in :user, @user
+
+    get :index
+    assert_response :success, "Should be successfully viewing admin/users"
+    assert_not_nil assigns(:chapters)
+  end
 end
