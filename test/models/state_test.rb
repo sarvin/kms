9040fdd39b2a77eccb10ffa15385660edc57f376
sarvin <scoachbuilder@dealersocket.com>
2016-01-17 20:42:54 -0600

    adding test for titleize_name method and scope

--- test/models/state_test.rb
+++ test/models/state_test.rb
@@ -1,12 +1,23 @@
 require 'test_helper'
 
 class StateTest < ActiveSupport::TestCase
-	#test "should not save article without title" do
-	#	state = State.all
-	#	puts state.inspect
-	#end
-
 	test "the truth" do
 		assert true
 	end
+
+  test "populated scope returns states that have chapters associated with them" do
+    states = State.populated
+
+    states.each do |state|
+      assert state.chapters.empty? == false, "State should have chapters associated with it"
+    end
+  end
+
+  test "titleize_name calls titleize on state name" do
+    name = "i am a name"
+    state = State.new
+    state.name = name
+
+    assert state.titleize_name == name.titleize
+  end
 end
