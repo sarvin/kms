904c771175c4cfa16fccb2f2830c6cac75aa60e5
sarvin <scoachbuilder@dealersocket.com>
2016-01-17 20:21:43 -0600

    fixed indentation to match ruby style guide, moved method declaration, scope and associations to match ruby style guide

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,14 +1,18 @@
 class Chapter < ActiveRecord::Base
-	validates :name, presence: true,
-		uniqueness: true
-	#validates :state_id, presence: true
+  belongs_to :state
 
-	belongs_to :state
+  has_one :page, :as => :pageable, dependent: :destroy
+  has_many :users, dependent: :nullify
+  #accepts_nested_attributes_for :page
 
-	has_one :page, :as => :pageable, dependent: :destroy
-	has_many :users, dependent: :nullify
-	#accepts_nested_attributes_for :page
+  validates :name, presence: true,
+    uniqueness: true
+  #validates :state_id, presence: true
 
-	scope :adopted, -> { where.not(state_id: nil) }
-	scope :orphaned, -> { where(state_id: nil) }
+  scope :adopted, -> { where.not(state_id: nil) }
+  scope :orphaned, -> { where(state_id: nil) }
+
+  def titleize_name
+    name.titleize
+  end
 end
