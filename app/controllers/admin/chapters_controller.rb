2fd8ea145fcf7bd41117d1de6e454f319a9553ed
sarvin <scoachbuilder@dealersocket.com>
2016-01-17 23:48:40 -0600

    indentation matches ruby style guide

--- app/controllers/admin/chapters_controller.rb
+++ app/controllers/admin/chapters_controller.rb
@@ -1,50 +1,50 @@
 class Admin::ChaptersController < Admin::BaseController
-	def new
-		@chapter = Chapter.new
-	end
+  def new
+    @chapter = Chapter.new
+  end
 
-	def create
-		@chapter = Chapter.new(chapter_params)
+  def create
+    @chapter = Chapter.new(chapter_params)
 
-		if @chapter.save
-			redirect_to [:admin, @chapter]
-		else
-			render 'new'
-		end
-	end
+    if @chapter.save
+      redirect_to [:admin, @chapter]
+    else
+      render 'new'
+    end
+  end
 
-	def index
+  def index
     @orphaned_chapters = Chapter.orphaned
     @adopted_chapters = Chapter.adopted
-	end
+  end
 
-	def edit
-		@chapter = Chapter.find(params[:id])
-	end
+  def edit
+    @chapter = Chapter.find(params[:id])
+  end
 
-	def show
-		@chapter = Chapter.find(params[:id])
-	end
+  def show
+    @chapter = Chapter.find(params[:id])
+  end
 
-	def update
-		@chapter = Chapter.find(params[:id])
+  def update
+    @chapter = Chapter.find(params[:id])
 
-		if @chapter.update(chapter_params)
-			redirect_to [:admin, @chapter]
-		else
-			render 'edit'
-		end
-	end
+    if @chapter.update(chapter_params)
+      redirect_to [:admin, @chapter]
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@chapter = Chapter.find(params[:id])
-		@chapter.destroy
+  def destroy
+    @chapter = Chapter.find(params[:id])
+    @chapter.destroy
 
-		redirect_to admin_chapters_path
-	end
+    redirect_to admin_chapters_path
+  end
 
-	private
-		def chapter_params
-			params.require(:chapter).permit(:name, :state_id)
-		end
+  private
+    def chapter_params
+      params.require(:chapter).permit(:name, :state_id)
+    end
 end
