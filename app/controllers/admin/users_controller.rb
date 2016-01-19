0dc470943cc40197cbaf93f45e40c5883cedc8c6
sarvin <scoachbuilder@dealersocket.com>
2016-01-19 14:56:51 -0600

    user#create accepts nested address model, test for user controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,6 +13,7 @@ class Admin::UsersController < Admin::BaseController
   def new
     @user = User.new
     @user.password = Devise.friendly_token.first(8)
+    @user.build_address
   end
 
   def edit
@@ -20,10 +21,20 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def create
-    @user = User.new(user_params)
+    user_attributes = user_params
+    address_attributes = user_attributes.delete(:address_attributes)
 
+    @user = User.new(user_attributes)
 
     if @user.save
+      ### build_address after saving user otherwise address will always be saved
+      ### regardless if columns_generated_by_user_populated?
+      @user.build_address(address_attributes)
+
+      if @user.address.columns_generated_by_user_populated?
+        @user.address.save
+      end
+
       redirect_to [:admin, @user]
     else
       render 'new'
@@ -40,15 +51,18 @@ class Admin::UsersController < Admin::BaseController
     end
   end
 
-  private
-    def user_params
-      ### Remove the password key of the params hash if it’s blank.
-      ### If not, Devise will fail to validate.
-      if params[:user][:password].blank?
-        params[:user].delete(:password)
-        params[:user].delete(:password_confirmation)
-      end
-
-      params.require(:user).permit(:name_first, :name_last, :chapter_id, :email, :password)
+private
+  def user_params
+    ### Remove the password key of the params hash if it’s blank.
+    ### If not, Devise will fail to validate.
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
     end
+
+    params.require(:user).permit(
+      :name_first, :name_last, :chapter_id, :email, :password,
+      address_attributes: [:line_1, :line_2, :city, :state_id, :postal_code]
+    )
+  end
 end
--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -26,4 +26,91 @@ class Admin::UsersControllerTest < ActionController::TestCase
 
     assert_select '#user_password', @user.password
   end
+
+  test "create action should create a new user" do
+    ### Arrange
+    user_attributes = self.class.user_attributes
+
+    ### need a user to be signed in before reaching admin interface
+    @user = User.take
+    @user.confirm
+    sign_in :user, @user
+
+    ### Act
+    stuff = post(
+      :create,
+      :user => user_attributes
+    )
+
+    new_user = User.find_by name_first: user_attributes[:name_first]
+    assert defined?(new_user), 'User should be created'
+  end
+
+  test "create action with no nested attributes should not create an address" do
+    ### Arrange
+    user_attributes = self.class.user_attributes
+
+    ### need a user to be signed in before reaching admin interface
+    @user = User.take
+    @user.confirm
+    sign_in :user, @user
+
+    ### Act
+    stuff = post(
+      :create,
+      :user => user_attributes
+    )
+
+    new_user = User.find_by name_first: user_attributes[:name_first]
+    assert defined?(new_user), 'User should be created'
+    assert_nil new_user.address, 'address should not return a persisted object'
+  end
+
+  test "create action with nested address should create a new user and address" do
+    ### Arrange
+    user_attributes = self.class.user_attributes
+    user_attributes[:address_attributes] = self.class.address_attributes
+
+    ### need a user to be signed in before reaching admin interface
+    @user = User.take
+    @user.confirm
+    sign_in :user, @user
+
+    line_1 = 'first line in address'
+
+    ### Act
+    stuff = post(
+      :create,
+      :user => user_attributes
+    )
+
+    new_user = User.find_by name_first: 'tim'
+
+    assert defined?(new_user), 'User should be created'
+    assert_equal new_user.address.line_1, user_attributes[:address_attributes][:line_1], "First line in address should equal #{line_1}"
+  end
+
+private
+
+  def self.user_attributes
+    chapter = Chapter.take
+
+    {
+      name_first: 'tim',
+      password: '12345678',
+      name_last: 'yyy',
+      email: 'foo@bar.com',
+      chapter_id: chapter.id,
+    }
+  end
+
+  def self.address_attributes
+    {
+      line_1: 'first line in address',
+      line_2: "",
+      city: "",
+      state_id: "",
+      postal_code: ""
+    }
+  end
 end
