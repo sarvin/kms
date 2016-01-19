6f1de9567d324362f2f16c7c4a84a542a2cea0df
sarvin <scoachbuilder@dealersocket.com>
2016-01-19 13:06:49 -0600

    model method and test for model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,23 @@
 class Address < ActiveRecord::Base
   belongs_to :state
   belongs_to :addressable, polymorphic: true
+
+  def self.columns_generated_by_user
+    important_fields = []
+    columns_generated_programmatically = {
+      id: true,
+      addressable_id: true,
+      addressable_type: true,
+      created_at: true,
+      updated_at: true
+    }
+
+    self.column_names.each do |column|
+      next if columns_generated_programmatically.has_key?(column.to_sym)
+
+      important_fields.push(column)
+    end
+
+    return important_fields
+  end
 end
--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "method columns_generated_by_user returns populated array" do
+    assert Address.columns_generated_by_user.any?, 'Array should be populated with values'
+  end
+
+  test "method columns_generated_by_user returns only columns in class" do
+    assert (Address.columns_generated_by_user - Address.column_names).empty?, 'Elements in array should only be found as columns in class'
+  end
+
+  test "method columns_generated_by_user does not returns all columns in model" do
+    assert Address.columns_generated_by_user.length, 'Array should be populated with values'
+    assert_operator Address.columns_generated_by_user.length, :<, Address.column_names.length, 'All columns in class should not be present'
+  end
 end
