ca91d8089198585c01133bd91f109fc594f01c9e
sarvin <scoachbuilder@dealersocket.com>
2016-01-23 23:52:22 -0600

    updated method to use self when accessing attribute, added three hash marks in front of comment, added rocye_roles array

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ActiveRecord::Base
     accepts_nested_attributes_for :address
 
   def name
-    "#{name_first} #{name_last}".titleize
+    "#{self[:name_first]} #{self[:name_last]}".titleize
   end
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  ### Include default devise modules. Others available are:
+  ### :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  royce_roles %w[president vice_president sgt_at_arms treasurer secretary road_captain member marauder]
 end
