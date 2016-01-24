1452342f62dda15055445a5f7129267d57994079
sarvin <scoachbuilder@dealersocket.com>
2016-01-23 23:35:20 -0600

    rearrange class to match rails style guide

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+	scope :orphaned, -> { where(chapter_id: nil) }
 
   belongs_to :chapter
   has_one :address, :as => :addressable
     accepts_nested_attributes_for :address
 
-	scope :orphaned, -> { where(chapter_id: nil) }
-
   def name
     "#{name_first} #{name_last}".titleize
   end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
