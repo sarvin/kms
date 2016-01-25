67a6175f3709458c10f326ca7c7aff21d6751044
sarvin <scoachbuilder@dealersocket.com>
2016-01-24 23:37:10 -0600

    sort users by role

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,24 @@
 module Admin::UsersHelper
+  def sort_users_by_role(users)
+    sorted = []
+    visited = {}
+
+    role_order.each do |role_name|
+      users.each do |user|
+        next if visited[user.id]
+
+        if user.has_role? role_name
+          sorted.push( user )
+
+          visited[user.id] = true
+        end
+      end
+    end
+
+    return sorted
+  end
+
+  def role_order
+    User.available_role_names
+  end
 end
