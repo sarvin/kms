d1b07fe5d2619f73a2a28fc36da487272c1a2fb5
sarvin <scoachbuilder@dealersocket.com>
2016-01-25 00:52:44 -0600

    playing around with sorting and scoped queries

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -11,8 +11,14 @@ class Chapter < ActiveRecord::Base
 
   scope :adopted, -> { where.not(state_id: nil) }
   scope :orphaned, -> { where(state_id: nil) }
+  #scope :sorted, -> { includes(:state).order("states.name or chapters.name ASC") }
 
   def titleize_name
     name.titleize
   end
+
+  #def self.sorted
+    #chapters = self.all.includes(:state)
+    #chapters.sort{|x,y| (x.state_id? ? x.state.name : x.name) <=> (y.state_id? ? y.state.name : y.name)}
+  #end
 end
