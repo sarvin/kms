202a27d1b74d7f5b4d2962e5052fbbfe7add8990
sarvin <scoachbuilder@dealersocket.com>
2016-01-28 00:45:32 -0600

    test for chapter

--- test/controllers/admin/chapters_controller_test.rb
+++ test/controllers/admin/chapters_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class Admin::ChaptersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  test "should get index" do
+    @user = sign_in_user(User.take)
+
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:orphaned_chapters)
+    assert_not_nil assigns(:adopted_chapters)
+  end
+
+  test "public navigation available on edit" do
+    @user = sign_in_user(User.take)
+    @chapter = Chapter.find_by(name:'SAMTAZ')
+    get :show, id: @chapter.id
+    assert_response :success
+    assert_not_nil assigns(:chapter)
+  end
+private
+  def sign_in_user(user)
+    user.confirm
+    sign_in :user, user
+
+    return user
+  end
 end
